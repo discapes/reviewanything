afe8fb1670ea2ba36ee28793ad54c240aa1c35e2
Miika Tuominen <miika.km.tuominen@gmail.com>
2024-03-22 17:20:03 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// this is only needed for the intellisense to work
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+      extend: {}
+    },
+    plugins: []
+  };
